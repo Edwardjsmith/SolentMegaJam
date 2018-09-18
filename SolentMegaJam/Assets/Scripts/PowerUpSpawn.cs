761f09ec631ad87baede5d2b27e8e5bee07e2ae9
OrganicJuiceMan <jackhill089@gmail.com>
2018-09-18 13:14:17 +0100

    Added images
    added some images and a scrip for the power up (dont have to use ut just there if you want it)

---
+++ SolentMegaJam/Assets/Scripts/PowerUpSpawn.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpSpawn : MonoBehaviour {
+    public float speed;
+    public GameObject PowerUp;
+    public int pooledPowerUp = 12;
+    List<GameObject> PowerUppool;
+    public Vector3 pos;
+    public GameObject GameManager;
+    // Use this for initialization
+    void Start () {
+        speed = 3;
+
+        PowerUppool = new List<GameObject>();
+        for (int i = 0; i < pooledPowerUp; i++);
+        {
+            GameObject obj = (GameObject)Instantiate(PowerUp);
+            obj.SetActive(false);
+            PowerUppool.Add(obj);
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(-speed * Time.deltaTime, 0, 0);
+        if (transform.position.x < -12f)
+        {
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(collision.gameObject);
+        PowerUp.SetActive(false);
+    }
+}
+

af4023f65eab030400c3a81f4d289f56b55f7693
StickNik <sticknik@ymail.com>
2018-09-18 15:37:37 +0100

    Scoreboard stuff

---
+++ SolentMegaJam/Assets/AddScore.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddScore : MonoBehaviour {
+
+    private int newscore;
+    private int highscore;
+
+    private int count = 0;
+
+	void Start () //on game end
+    {
+        //newscore = final score from game once game has ended
+        //newscore = PlayerPrefs.GetInt("NewScore")
+	}
+	
+	void Update ()
+    {
+        while (true)
+        {
+            highscore = PlayerPrefs.GetInt("Score"+count, 0);
+
+            if (newscore > highscore)
+            {
+                PlayerPrefs.SetInt("Score"+count, newscore);
+                //PlayerPrefs.SetString("Name"+count, name input)
+                count = 0;
+            }
+            else
+            {
+                count++;
+            }
+        }
+	}
+}
---
+++ SolentMegaJam/Assets/Scoreboard.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scoreboard : MonoBehaviour {
+
+    private int highscore;
+    private string highname;
+
+    private int count = 0;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        highscore = PlayerPrefs.GetInt("Score" + count, 0);
+        highname = PlayerPrefs.GetString("Name" + count);
+    }
+}
